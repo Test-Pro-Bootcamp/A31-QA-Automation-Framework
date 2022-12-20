6b22dc52c8c18f7946550eae2027351ed4a99ba6
czar-testprio <czar@testpro.io>
2022-12-20 22:21:57 +0800

    Commit webdriver manager config
    committing of webdriver manager config

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 
 public class BaseTest {
 
+    static WebDriver driver;
+
     @BeforeSuite
-    public static void chromeConfigs() {
-        // This is for Windows users
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeMethod
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests {
+public class LoginTests extends BaseTest {
 
     @Test
     public static void LoginEmptyEmailPasswordTest () {
 
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
