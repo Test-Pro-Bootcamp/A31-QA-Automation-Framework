4c3541c15d018952c7d61669daff29eccdf5e4da
nicoleliano <nicole.liano@testpro.io>
2022-12-26 16:29:09 -0500

    Intro to Selenium Notes

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -16,6 +18,57 @@ public class LoginTests extends BaseTest {
         String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
+
+    @Test
+    public static void LoginInvalidEmail(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("type=['email']"));
+        emailField.click();
+        emailField.sendKeys("cucarach@class.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("type=[password]"));
+        passwordField.click();
+        passwordField.sendKeys("yayayaya");
+
+        WebElement loginButton = driver.findElement(By.cssSelector("type=['submit']"));
+        loginButton.click();
+
+        Assert.assertTrue(emailField.isDisplayed());
+
+        driver.quit();
+
+    }
+
+    @Test
+    public static void LoginValidEmailPasswordTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("demo@class.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
+
+        WebElement loginField = driver.findElement(By.cssSelector("[type='submit']"));
+        loginField.click();
+
+        WebElement avatar = driver.findElement(By.className("avatar"));
+        Assert.assertTrue(avatar.isDisplayed());
+
         driver.quit();
     }
 }
