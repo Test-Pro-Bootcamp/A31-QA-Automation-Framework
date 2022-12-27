3aa6f30e8de4ab9635de03a9cd25f02f76b565ae
JarrodDennis <jarrod.dennis@testpro.io>
2022-12-26 21:28:57 -0500

    Verify User can Navigate to Registration Page

--- src/test/java/Homework14Jwd.java
+++ src/test/java/Homework14Jwd.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,12 +12,13 @@ public class Homework14jwd(){
     @Test
 
         public static void navigation() {
-        WebDriver register = new WebDriver();
+
+        WebDriver register = new ChromeDriver();
         register.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String url = "https://testpro.io/";
         register.get(url);
 
-        WebElement registrationLink = register.findElement(By.cssSelector(#hel));
+        WebElement registrationLink = register.findElement(By.cssSelector(a[type = 'submit']);
 
         register.findElement(By.cssSelector(registrationLink)).click();
 
