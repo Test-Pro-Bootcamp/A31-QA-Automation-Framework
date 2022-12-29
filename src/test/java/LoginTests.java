6f41a667fdcaa9569fe209f302d1a424b3e52dfc
szandragecse <szandra.gecse@testpro.io>
2022-12-28 20:40:31 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -13,9 +13,10 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://bbb.testpro.io/";
+        String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+        //changes from get1-intro
     }
 }
