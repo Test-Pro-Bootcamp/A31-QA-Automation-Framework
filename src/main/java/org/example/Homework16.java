033c3418248011ff7749fc145529830e551c6234
TatGur <tatsiana.guryev@testpro.io>
2022-12-29 11:35:31 -0600

    Create Homework16.java

---
+++ src/main/java/org/example/Homework16.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class LoginTests extends BaseTest {
+
+
+@Test
+public static void Navigation() {
+
+public static void LoginEmptyEmailPasswordTest()  {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        Thread.sleep(2000);
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Thread.sleep(2000);
+        driver.quit();
+        }
+
