2833ee2cd1733cd65f7ebf15ea57903f0acc0421
Simplyd2d <121208284+SimplyDD@users.noreply.github.com>
2022-12-29 11:05:38 -0500

    Created file with updates
    Created Homework16 file
    Updated TestNG class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 
---
+++ src/test/java/Homework16.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest {
+
+    @Test
+    public void Navigation () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
+        registrationLink.click();
+        //Thread. sleep (200);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        String registrationURL = "https://bbb.testpro.io/resgistration.php";
+        Assert.assertEquals(driver.getCurrentUrl(),registrationURL);
+        driver.quit();
+
+    }
+}
