37cbbc5934a2237f19e1ede8d8343a3faac72a4f
Roman Rykov <Roman.Rykov@testpro.io>
2022-12-29 21:44:04 -0500

    Homework#16 Roman Rykov

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,5 +19,27 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+    }
+
+    @Test
+    public static void RegistrationLinkNavigationTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
+        registrationLink.click();
+
+
+        WebElement title = driver.findElement(By.cssSelector("[style='padding: 32px 0; text-align: center;']"));
+        Assert.assertTrue(title.isDisplayed());
+
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
     }
 }
