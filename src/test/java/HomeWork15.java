833d778a4081398b3ea8335f4d375c7a02d5e71b
JarrodDennis <jarrod.dennis@testpro.io>
2022-12-30 23:00:10 -0500

    Verify User can Navigate to Registration Page

---
+++ src/test/java/HomeWork15.java
@@ -0,0 +1,32 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class HomeWork15 {
+
+    @Test
+
+    public static void navigation() {
+        WebDriver register = new WebDriver();
+        register.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://testpro.io/";
+        register.get(url);
+
+        WebElement registtrationLink = register.findElement(By.cssSelector(#hel));
+
+        String registrationLink= "#hel";
+        register.findElement(By.cssSelector(registrationLink)).click();
+
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(register.getCurrentUrl(), registrationUrl);
+        register.quit();
+
+
+    }
+
+
+}
