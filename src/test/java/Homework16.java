1936766c4346a13e22d577bbfa1acdeef4614d5c
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-03 15:32:41 -0500

    moving login, email and submit to methods

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -26,18 +26,28 @@ public class Homework16 extends BaseTest {
         String url = "https://bbb.testpro.io/";
         driver.get(url);
 
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.sendKeys("demo.class.com");
-
-        WebElement passwordField = driver.findElement(By.cssSelector("[type ='password']"));
-        passwordField.sendKeys("te$t$tudent");
-
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type=submit]"));
-        submitButton.click();
+        provideEmail();
+        providePassword();
+        clickSubmit();
 
         WebElement avataricon = driver.findElement(By.cssSelector("img.avatar"));
 
         Assert.assertTrue(avataricon.isDisplayed());
 
 }
+
+    public static void clickSubmit() {
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type=submit]"));
+        submitButton.click();
+    }
+
+    public void providePassword() {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type ='password']"));
+        passwordField.sendKeys("te$t$tudent");
+    }
+
+    public static void provideEmail() {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.sendKeys("demo.class.com");
+    }
 }
