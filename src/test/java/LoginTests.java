9d5a5b5b98dfc4990b66cebf472f6aeb577dd05f
Napellaniz <natalie.apellaniz@testpro.io>
2023-01-02 20:19:35 -0600

    Update LoginTests.java
    homework #15 updates

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -13,10 +13,30 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://bbb.testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
         //changes from Natalies Branch
+    }
+
+    @Test
+    public static void LoginValidEmailPasswordTest() {
+        //Precondition: Chrome Browser should be opened
+        //Step 1:Open Koel login page
+        //Step 2:Enter existing username
+        //Step 3;Enter correct password
+        //Step 4:Click login button
+        //Expected Results:User should be directed to the home page
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("");
+        //type text in emailfield
     }
 }
