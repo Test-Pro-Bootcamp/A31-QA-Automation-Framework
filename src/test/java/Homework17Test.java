1917f72da63c9ea40e8bfdf8ea7c2acd482dbac9
JarrodDennis <jarrod.dennis@testpro.io>
2023-01-02 19:39:08 -0500

    Verify User can Navigate to Registration Page

--- src/test/java/Homework17Test.java
+++ src/test/java/Homework17Test.java
@@ -1,4 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+
 public class Homework17Test extends BaseTest {
+
+@Test
+public void validLoginEmailPasswordTest(){
+    openLoginURL();
+    enterEmail();
+    enterPassword();
+    clickButton();
+    WebElement avatarIcon =driver.findElement(By.cssSelector("[alt='Avatar of student']"));
+    Assert.assertTrue(avatarIcon.isDisplayed());
+}
 
 
 }
