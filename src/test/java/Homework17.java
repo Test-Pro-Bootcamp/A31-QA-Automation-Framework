51eff612e89f7c3951298364ea3e8e94f3f7f44f
abdulkadiral <121259833+abdulkadiral@users.noreply.github.com>
2023-01-03 00:30:42 -0600

    Create Homework17.java

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest {
+
+    @Test
+    public static void addSongToPlaylist() throws InterruptedException {
+
+        navigateToPage();
+
+        provideEmail("adeagle2021@gmail.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+
+        Thread.sleep(2000);
+
+
+
+
+    }
+
+
+
+
+
+    }
