6f7d6cf5084f9f8a3875e2b4f02d1ec611625936
Yevgeny5 <121211452+Yevgeny5@users.noreply.github.com>
2023-01-02 18:45:41 -0800

    Homework17-YG
    Add song to playlist.

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -14,15 +14,13 @@ public class Homework17 extends BaseTest {
         enterEmail("jenyag5@gmail.com");
         enterPassword("te$t$tudent");
         clickSubmit();
-
-        WebDriver driver;
-
-
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        allSongs();
+        songName();
+        addTo();
+        playlistAuto();
+        Assert.assertTrue(getSuccessPopUp().isDisplayed());
         }
-
-        public void openloginUrl() {
+    public void openloginUrl() {
         driver.get("http://bbb.testpro.io/");
         }
         public void enterEmail(String email) {
@@ -35,8 +33,28 @@ public class Homework17 extends BaseTest {
         passwordField.click();
         passwordField.sendKeys(password);
         }
-        public void clickSubmit() {
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        submitButton.click();
+        public void allSongs() throws InterruptedException {
+        WebElement allSongs = driver.findElement(By.cssSelector("#sidebar > section.music > ul > li:nth-child(3) > a"));
+        allSongs.click();
+        Thread.sleep(2000);
         }
+        public void songName() throws InterruptedException {
+            WebElement songName = driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"));
+            songName.click();
+            Thread.sleep(2000);
         }
+        public void addTo() throws InterruptedException {
+            WebElement addTo = driver.findElement(By.cssSelector("button[class='btn-add-to']"));
+            addTo.click();
+            Thread.sleep(2000);
+        }
+        public void playlistAuto() throws InterruptedException {
+            WebElement playlistAuto = driver.findElement(By.cssSelector("#songsWrapper > header > div.song-list-controls > div > section.existing-playlists > ul > li:nth-child(5)"));
+            playlistAuto.click();
+            Thread.sleep(4000);
+        }
+        public WebElement getSuccessPopUp() {
+            return driver.findElement(By.cssSelector("div[class='success show']"));
+        }
+}
+
