adc142dbf982a22070b9ddb94c1fd175029acbc3
PoojaB05 <pooja.b.bankar@gmail.com>
2023-01-03 18:48:30 -0500

    Create Homework17_PB.java

---
+++ src/test/java/Homework17_PB.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework17_PB {
+
+
+    /* 1.Create a new file Homework17.java with @Test annotation in IntelliJ IDEA.
+       2.Create a test case addSongToPlaylist() using @Test annotations and the helper/reusable methods we created.
+       3. Navigate to "https://bbb.testpro.io/"
+       4. Log in with your credentials.
+       5. Add a song to a playlist.
+       6. Validate that there is a song added.
+       7. Create a new branch and commit your changes.
+       8. Push your code to a remote repository.
+       9. Create a pull request.*/
+
+    webDriver driver = new ChromeDriver();
+}
