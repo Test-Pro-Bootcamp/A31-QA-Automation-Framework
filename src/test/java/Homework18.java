610022866428e2af14218518be604f39ee41d62a
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-04 14:28:19 -0600

    Update Homework18.java

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,2 +1,25 @@
-public class Homework18 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework18 extends BaseTest {
+
+    @Test
+    public static void playSongTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        
+
+        driver.quit();
+
+    }
 }
