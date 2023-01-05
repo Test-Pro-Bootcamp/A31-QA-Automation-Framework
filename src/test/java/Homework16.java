c2c2f2452f16beb5b2942ba593254dcde5550977
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-04 20:55:49 -0500

    adding a song to a playlist

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -31,14 +31,27 @@ public class Homework16 extends BaseTest {
         WebElement avataricon = driver.findElement(By.cssSelector("img.avatar"));
 
         Assert.assertTrue(avataricon.isDisplayed());
+    }
+//    @Test(enabled = true, description = "ch17homeworkaddsong")
+//    public static void addSong(){
+//
+//        navigateToPage();
+//        provideEmail("kbetestack@gmail.com");
+//        providePassword("te$t$tudent");
+//        clickSubmit();
+//
+//        WebElement allSongs = driver.findElement(By.className(".songs active"));
+//        allSongs.click();
+//
+//        WebElement chooseSong = driver.findElement(By.xpath(//td[contains( text(),'Frantic')])) ;
+//        chooseSong.click();
+//
+//        WebElement addToPlaylist = driver.findElement(By.className(".btn-add-to"));
+//        addToPlaylist.click();
+//
+
 
     }
+
+
 }
-
-   // public static void  (){
-   // navigateToPage();
-   // provideEmail("demo@class.com");
-   //providePassword("te$t$tudent");
-// clickSubmit();
-
-
