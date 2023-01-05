cc0c4f4ca0a52e934349ac1840f5bbd8c9190a4c
JohnyBrt <burtoiuionut@ymail.com>
2023-01-04 20:34:52 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,20 +1,24 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTests extends BaseTest {
 
 
     @Test(enabled = true, priority = 0, description = "LoginEmptyEmailPasswordTest")
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void LoginEmptyEmailPasswordTest() {
 
         navigateToPage();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
 
     @Test(enabled = true, priority = 1, description = "LoginValidEmailValidPasswordTest")
-    public static void LoginValidEmailValidPasswordTest () throws InterruptedException {
+    public static void LoginValidEmailValidPasswordTest() throws InterruptedException {
 
         navigateToPage();
 
@@ -29,7 +33,7 @@ public class LoginTests extends BaseTest {
     }
 
     @Test(enabled = true, priority = 2, description = "LoginInvalidEmailValidPasswordTest")
-    public static void LoginInvalidEmailValidPasswordTest () {
+    public static void LoginInvalidEmailValidPasswordTest() {
 
         navigateToPage();
 
@@ -42,7 +46,7 @@ public class LoginTests extends BaseTest {
     }
 
     @Test(enabled = true, priority = 3, description = "LoginValidEmailValidPasswordTest")
-    public static void updateProfileNameTest () throws InterruptedException {
+    public static void updateProfileNameTest() throws InterruptedException {
 
         navigateToPage();
 
@@ -65,5 +69,23 @@ public class LoginTests extends BaseTest {
 
     }
 
+    public class Homework16 extends BaseTest {
 
+        @Test
+        public void Navigation() {
+
+
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io";
+            driver.get(url);
+            WebElement registrationLink = driver.findElement(By.cssSelector("[id='hell']"));
+            registrationLink.click();
+
+            String registrationUrl = "https://bbb.testpro.io/registration.php";
+            Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+            driver.quit();
+        }
+    }
 }
