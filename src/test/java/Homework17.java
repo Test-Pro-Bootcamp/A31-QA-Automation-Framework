898eec4028b6b6b8fa817b6cd93aef05b0354b02
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-05 17:19:19 -0500

    add song to playlist
    added Frantic to first custom playlist

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -10,21 +10,35 @@ public class Homework17 extends BaseTest {
 
 
     @Test(enabled = true, description = "ch17homeworkaddsong")
-    public static void addSong(){
+    public void addSongToPlaylist()  {
 
+        //goto Koel page-uname and pw-submit
         navigateToPage();
        provideEmail("kbetestack@gmail.com");
        providePassword("te$t$tudent");
         clickSubmit();
 
+        //find and click on All Songs button
         WebElement allSongs = driver.findElement(By.className(".songs active"));
         allSongs.click();
 
-        WebElement chooseSong = driver.findElement(By.xpath(//td[contains( text(),'Frantic')])) ;
+        //find the song 'Frantic' and click on it
+        WebElement chooseSong = driver.findElement(By.xpath("//td[contains(text(),'Frantic')]")) ;
         chooseSong.click();
 
+        //find add to button and click
         WebElement addToPlaylist = driver.findElement(By.className(".btn-add-to"));
        addToPlaylist.click();
+
+      //selecting a playlist to add song to and choose
+       WebElement existPlaylist = driver.findElement(By.cssSelector("#songsWrapper li.playlist"));
+       existPlaylist.click();
+
+       //confirm song in playlist
+        WebElement addedSong = driver.findElement(By.cssSelector("#songsWrapper li.playlist"));
+        Assert.assertTrue(addedSong.isDisplayed());
+
+
 
 
 }
