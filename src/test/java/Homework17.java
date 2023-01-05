355f5c88540cbd9e208cef2d8bb0403d9a36c7a9
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-05 18:43:29 -0500

    fix Ammend statement

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -10,35 +10,33 @@ public class Homework17 extends BaseTest {
 
 
     @Test(enabled = true, description = "ch17homeworkaddsong")
-    public void addSongToPlaylist()  {
+    public void addSongToPlaylist() {
 
         //goto Koel page-uname and pw-submit
         navigateToPage();
-       provideEmail("kbetestack@gmail.com");
-       providePassword("te$t$tudent");
+        provideEmail("kbetestack@gmail.com");
+        providePassword("te$t$tudent");
         clickSubmit();
 
         //find and click on All Songs button
-        WebElement allSongs = driver.findElement(By.className(".songs active"));
+        WebElement allSongs = driver.findElement(By.cssSelector("[class=songs]"));
         allSongs.click();
 
         //find the song 'Frantic' and click on it
-        WebElement chooseSong = driver.findElement(By.xpath("//td[contains(text(),'Frantic')]")) ;
+        WebElement chooseSong = driver.findElement(By.xpath("//td[contains(text(),'Frantic')]"));
         chooseSong.click();
 
         //find add to button and click
-        WebElement addToPlaylist = driver.findElement(By.className(".btn-add-to"));
-       addToPlaylist.click();
+        WebElement addToPlaylist = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addToPlaylist.click();
 
-      //selecting a playlist to add song to and choose
-       WebElement existPlaylist = driver.findElement(By.cssSelector("#songsWrapper li.playlist"));
-       existPlaylist.click();
+        //selecting a playlist to add song to and choose
+        WebElement existPlaylist = driver.findElement(By.cssSelector("#songsWrapper li.playlist"));
+        existPlaylist.click();
 
-       //confirm song in playlist
-        WebElement addedSong = driver.findElement(By.cssSelector("#songsWrapper li.playlist"));
-        Assert.assertTrue(addedSong.isDisplayed());
+        //confirm song in playlist
+       WebElement addedSong = driver.findElement(By.cssSelector("a[href*='22369']"));
+       Assert.assertTrue(addedSong.isDisplayed());
 
-
-
-
+    }
 }
