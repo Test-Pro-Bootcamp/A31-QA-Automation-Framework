12a9dd19b513a07be243182e7dca23a907c0033a
abdulkadiral <121259833+abdulkadiral@users.noreply.github.com>
2023-01-06 19:34:01 -0600

    Update Homework17.java

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,44 +1,31 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
 
 
     @Test
-    public static void loginPage() throws InterruptedException {
+    public static void addSongToPlaylist() throws InterruptedException {
 
         navigateToPage();
         logintoPage();
-        updateProfile();
-        Thread.sleep(2000);
-
-
-
-
-
-        Thread.sleep(2000);
-
-
-        WebElement newPassword = driver.findElement(By.cssSelector("[name='new_password']"));
-        newPassword.click();
-        newPassword.sendKeys("teststudent");
-
-        WebElement saveButton = driver.findElement(By.cssSelector("[class='btn-submit']"));
-        saveButton.click();
-
-
-
-
-
-
+        selectSong();
+       Actions actions = new Actions(driver);
+        WebElement songTitle = driver.findElement(By.xpath("//tr[@draggable='true']//td[2]"));
+        songTitle.click();
+       actions.contextClick(songTitle).perform();
+       WebElement addtoBtn = driver.findElement(By.cssSelector("[class='has-sub']"));
+       Thread.sleep(2000);
+       actions.moveToElement(addtoBtn).perform();
+       WebElement playlist = driver.findElement(By.xpath("//ul[@class='menu submenu menu-add-to']/li[7]"));
+       playlist.click();
+       Thread.sleep(2000);
 
 
     }
 
 
-
-
-
-
-    }
+}
