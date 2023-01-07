6588bccef5497c3ae1fbc520a233fbcb7de4ec6a
PoojaB05 <pooja.b.bankar@gmail.com>
2023-01-06 19:17:15 -0500

    Create Homework18_PB.java

---
+++ src/test/java/Homework18_PB.java
@@ -0,0 +1,19 @@
+public class Homework18_PB extends BaseTest{
+    /*
+    1. Create a new file Homework18.java with @Test annotation in IntelliJ IDEA.
+    2. Create a test case playSong() using @Test annotations.
+    3. Use the helper/reusable methods we created.
+    4. Create a new config XML file for the test case.
+    5. Specify the method to be ran in the XML file.
+    6. Navigate to "https://bbb.testpro.io/".
+    7. Log in with your credentials.
+    8. Play a song.
+    9. Validate that a song is playing.
+    10. Create a new branch and commit your changes.
+    11. Push your code to a remote repository.
+    12. Create a pull request.*/
+
+
+    
+
+}
