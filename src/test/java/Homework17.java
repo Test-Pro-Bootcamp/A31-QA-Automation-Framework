78c49d8f8b30f98a7003fa2dad0d47be7cbca431
abdulkadiral <121259833+abdulkadiral@users.noreply.github.com>
2023-01-06 18:08:49 -0600

    Update Homework17.java

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,33 +1,35 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
+
 
     @Test
     public static void loginPage() throws InterruptedException {
 
         navigateToPage();
         logintoPage();
-
+        updateProfile();
         Thread.sleep(2000);
 
-        WebElement allSongs = driver.findElement(By.cssSelector("a[class=\"songs\"]"));
-        allSongs.click();
 
-        Thread.sleep(2000);
 
-        WebElement songTitle = driver.findElement(By.xpath("//td[contains(text(), 'Take my Hand')]"));
-        songTitle.click();
+
 
         Thread.sleep(2000);
 
 
-        WebElement addTo = driver.findElement(By.cssSelector("[data-test='add-to-btn']"));
-        addTo.click();
+        WebElement newPassword = driver.findElement(By.cssSelector("[name='new_password']"));
+        newPassword.click();
+        newPassword.sendKeys("teststudent");
 
-        System.out.println("SFSAJ");
+        WebElement saveButton = driver.findElement(By.cssSelector("[class='btn-submit']"));
+        saveButton.click();
+
+
+
+
 
 
 
