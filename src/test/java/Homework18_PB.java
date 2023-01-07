97f35b6f20d631168dbaec1a3ada5e15029b96b2
PoojaB05 <pooja.b.bankar@gmail.com>
2023-01-07 15:51:13 -0500

    Play song method with xml file and adding reverence to gradle

--- src/test/java/Homework18_PB.java
+++ src/test/java/Homework18_PB.java
@@ -1,3 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Homework18_PB extends BaseTest{
     /*
     1. Create a new file Homework18.java with @Test annotation in IntelliJ IDEA.
@@ -14,6 +20,36 @@ public class Homework18_PB extends BaseTest{
     12. Create a pull request.*/
 
 
-    
+    @Test
+    public void playSongTest() throws InterruptedException{
 
+        navigateToPage();
+        login("pooja.bankar@testpro.io", "te$t$tudent");
+        clickSubmit();
+        clickAllSongsTab();
+        selectAndPlaySong();
+        isSongPlaying();
+    }
+
+  //Locate All Songs
+           public void clickAllSongsTab() throws InterruptedException {
+               WebElement allSongsTabField = driver.findElement(By.cssSelector("a.songs"));
+                allSongsTabField.click();
+                Thread.sleep(3000);
+       }
+
+        // Locate first song from All Songs List,double click and play
+        public void selectAndPlaySong() throws InterruptedException{
+
+            Actions actions = new Actions(driver);
+            WebElement selectFirstSongField = driver.findElement(By.xpath("//*[@id='songsWrapper']//div[1]/table/tr[1]/td[2]"));
+            actions.doubleClick(selectFirstSongField).build().perform();
+           Thread.sleep(3000);
+       }
+
+       //Validate song is playing
+        public void isSongPlaying(){
+         WebElement soundbar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+            Assert.assertTrue(soundbar.isDisplayed());
+        }
 }
