6816ec8f73795f64881f3f82246320b52b5041fa
GreeshmaUdupa <121182336+GreeshmaUdupa@users.noreply.github.com>
2023-01-08 20:16:54 -0600

    updated Homework19 file

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -10,17 +10,33 @@ public class Homework19 extends BaseTest {
         login("test123@test.com", "te$t$tudent");
         Thread.sleep(2000);
 
+        createPlaylist();
         selectDesiredPlaylist();
         deleteDesiredPlaylist();
+        deletionMsg();
+    }
 
-        WebElement deletePlaylistMsg = getDeleteMsgDisplayed();
-        Assert.assertTrue(deletePlaylistMsg.isDisplayed());
+    //Create a playlist
+    public void createPlaylist() throws InterruptedException {
+        String playlistName = "HW19";
+        WebElement clickPlusButton = driver.findElement(By.cssSelector("i[class='fa fa-plus-circle create']"));
+        clickPlusButton.click();
+        Thread.sleep(2000);
+
+        WebElement clickNewPlaylistButton = driver.findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
+        clickNewPlaylistButton.click();
+        Thread.sleep(2000);
+
+        WebElement newPlaylistField = driver.findElement(By.cssSelector("input[name='name']"));
+        newPlaylistField.sendKeys(playlistName);
+        newPlaylistField.submit();
         Thread.sleep(2000);
     }
 
     //Navigate and select a playlist
     public void selectDesiredPlaylist() throws InterruptedException {
-        WebElement selectAPlaylist = driver.findElement(By.cssSelector("[href='#!/playlist/35734']"));
+        String playlistName = "HW19";
+        WebElement selectAPlaylist = driver.findElement(By.xpath("//section[@id='playlists']//li[@class='playlist playlist']//a[contains(text(),'" + playlistName + "')]"));
         selectAPlaylist.click();
         Thread.sleep(2000);
     }
@@ -33,7 +49,9 @@ public class Homework19 extends BaseTest {
     }
 
     //Validate the playlist is deleted
-    public WebElement getDeleteMsgDisplayed() throws InterruptedException {
-        return driver.findElement(By.cssSelector("[class='success show']"));
+    public void deletionMsg() throws InterruptedException {
+        WebElement successfulDeleteMsg = driver.findElement(By.cssSelector("[class='success show']"));
+        Assert.assertTrue(successfulDeleteMsg.isDisplayed());
     }
 }
+
