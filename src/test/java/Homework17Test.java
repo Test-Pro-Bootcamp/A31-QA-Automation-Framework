5585a549e68b6a46cef9f6d11be91d10832a0915
JarrodDennis <jarrod.dennis@testpro.io>
2023-01-09 18:59:28 -0500

    Reworking assignment 17

--- src/test/java/Homework17Test.java
+++ src/test/java/Homework17Test.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
@@ -11,7 +12,7 @@ public class Homework17Test extends BaseTest {
         String url = grabUrl();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
-    @Test(priority = 1, description = "Log in with Valid User Info")
+    @Test(enabled = false, description = "Log in with Valid User Info")
      public void loginValidEmailPasswordTest(){
             grabUrl();
         provideEmail("skyeman75@gmail.com");
@@ -22,7 +23,7 @@ public class Homework17Test extends BaseTest {
          WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student']"));
          assertTrue(avatarIcon.isDisplayed());
     }
-    @Test(priority = 2, description = "Invalid User Email")
+    @Test( enabled = false, description = "Invalid User Email")
     public void loginInvalidEmailPasswordTest(){
        grabUrl();
         String url = grabUrl();
@@ -31,6 +32,25 @@ public class Homework17Test extends BaseTest {
         clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
+    @Test(priority = 1, description = "Move a Song to a Play List")
+    public void addASong(){
+        grabUrl();
+        provideEmail("skyeman75@gmail.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        WebElement allSongs = driver.findElement(By.cssSelector("a[href='#!/songs']"));
+        allSongs.click();
+        WebElement selectSong = driver.findElement(By.xpath("//tr[@class='song-item selected']"));
+        selectSong.click();
+        WebElement playList = driver.findElement(By.cssSelector("a[href='#!/playlist/35821']"));
+        Actions act = new Actions(driver);
+        act.dragAndDrop(selectSong,playList).build().perform();
+
+
+    }
+
+
+
 
 
 }
