14c626599cae0f287c8074a12d327c0c5d2174e3
szandrag <121212425+szandrag@users.noreply.github.com>
2023-01-08 23:14:29 -0500

    HWork18

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,9 +1,4 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import java.time.Duration;
 
 public class LoginTests extends BaseTest {
 
