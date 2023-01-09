1abba4a326707f4780119d0595e7ce748c481abf
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-08 19:39:01 -0500

    hw ch 19  delete playlist

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,4 +1,18 @@
-public class Homework19 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
-    
+public class Homework19 extends BaseTest {
+@Test
+public void deletePlaylist () {
+
+    login("kbetestack@gmail.com", "te$t$tudent");
+    WebElement getPlaylist = driver.findElement(By.cssSelector("a[href*='22390']"));
+    getPlaylist.click();
+    WebElement delPlaylist = driver.findElement(By.cssSelector(".del btn-delete-playlist"));
+    delPlaylist.click();
+
+
+    }
+
 }
