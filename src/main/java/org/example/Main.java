ad80816533ba2d4397e5c6afb7b976d85ac8d194
shalinibaronia <50969029+shalinibaronia@users.noreply.github.com>
2023-01-09 12:17:48 -0800

    Homework16

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -5,18 +5,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class Homework16 extends BaseTest {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void RegistrationNavigation () {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://bbb.testpro.io/";
         driver.get(url);
+
+        WebElement registration = driver.findElement(By.id("hel"));
+        registration.click();
+       WebElement button = driver.findElement(By.id("button"));
+        Assert.assertTrue(button.isDisplayed());
 
         driver.quit();
     }
