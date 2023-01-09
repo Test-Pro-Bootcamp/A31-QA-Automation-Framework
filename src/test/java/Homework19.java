25e0cd956ae0a6c1fc3927379688ee50552692bb
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-01-08 17:54:22 -0800

    Homework 19 - Jimmy Vu (minor update)
    committed change for variable name to camelcase

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -9,22 +9,22 @@ public class Homework19 extends BaseTest{
 
     @Test(enabled = true, priority = 1, description = "delete a playlist test")
     @Parameters({"PlaylistToDelete"})
-    public static void deleteAPlaylist(String PlaylistToDelete) throws InterruptedException {
+    public static void deleteAPlaylist(String playlistToDelete) throws InterruptedException {
         //parameterization for baseURL is in BaseTests launchBrowser beforemethod already,
         //I added a playlist parameter in TestNG.xml just to play with it
         //but for the homework the baseURL parameterization is already in BaseTest launchBrowser method!
         login("jimmypvu@gmail.com", "te$t$tudent");
         Thread.sleep(750);
 
-        createTestPlaylist(PlaylistToDelete);
+        createTestPlaylist(playlistToDelete);
         Thread.sleep(750);
 
-        deleteTestPlaylist(PlaylistToDelete);
+        deleteTestPlaylist(playlistToDelete);
         Thread.sleep(750);
 
     }
 
-    public static void createTestPlaylist(String PlaylistToDelete) throws InterruptedException{
+    public static void createTestPlaylist(String playlistToDelete) throws InterruptedException{
         WebElement addPlaylistBtn = driver.findElement(By.xpath("//i[@data-testid='sidebar-create-playlist-btn']"));
         addPlaylistBtn.click();
         Thread.sleep(750);
@@ -36,14 +36,14 @@ public class Homework19 extends BaseTest{
         WebElement newPlaylistNameField = driver.findElement(By.xpath("//form[@class='create']//input"));
         newPlaylistNameField.click();
         Thread.sleep(500);
-        newPlaylistNameField.sendKeys(PlaylistToDelete);
+        newPlaylistNameField.sendKeys(playlistToDelete);
         Thread.sleep(500);
         newPlaylistNameField.sendKeys(Keys.ENTER);
         Thread.sleep(750);
     }
 
-    public static void deleteTestPlaylist(String PlaylistToDelete) throws InterruptedException {
-        WebElement playlist = driver.findElement(By.xpath("//li[@class='playlist playlist']//a[contains(text(), '"+PlaylistToDelete+"')]"));
+    public static void deleteTestPlaylist(String playlistToDelete) throws InterruptedException {
+        WebElement playlist = driver.findElement(By.xpath("//li[@class='playlist playlist']//a[contains(text(), '"+playlistToDelete+"')]"));
         playlist.click();
         Thread.sleep(750);
 
