c480070b497394e2ed22062cfd93946a8ea9debd
Myeonghoe <myeonghoe.son@testpro.io>
2023-01-11 10:33:50 -0800

    import

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,2 +1,15 @@
-public class Homework17 {
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest {
+
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
+        openLogUrl();
+        login();
+        createPlaylist();
+        addSongToThePlaylist();
+        validateSongIsAdded();
+    }
+
+
 }
