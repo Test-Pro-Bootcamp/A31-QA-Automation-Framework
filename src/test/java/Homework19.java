e747d5c1f188d25d04293efe872f08f0e492c3d8
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-11 17:36:36 -0500

    Update Homework19.java

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class Homework19c extends BaseTest {
+public class Homework19 extends BaseTest {
 @Test
 public void deletePlaylist () {
 
