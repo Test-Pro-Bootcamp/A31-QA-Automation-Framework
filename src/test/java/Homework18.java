7a946577b1f0277f9f329f3fd524d30fe8dbec9a
PGgit1 <121201098+PGgit1@users.noreply.github.com>
2023-01-11 17:44:24 -0800

    Update Homework18.java

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,14 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 public class Homework18 extends BaseTest{
     @Test
     public void playSong() throws InterruptedException{
         navigateToPage();
         login("pavelguyvan@gmail.com", "te$t$tudent1");
-        Thread.sleep(2000);
         allSongs();
         songName();
         playNextButton();
@@ -16,27 +17,23 @@ public class Homework18 extends BaseTest{
         soundBars();
     }
     public void allSongs() throws InterruptedException{
-        WebElement allSongs = driver.findElement(By.cssSelector("#sidebar > section.music > ul > li:nth-child(3) > a"));
+        WebElement allSongs = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#sidebar > section.music > ul > li:nth-child(3) > a")));
         allSongs.click();
-        Thread.sleep(2000);
     }
     public void songName() throws InterruptedException{
-        WebElement songName = driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"));
+        WebElement songName = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]")));
         songName.click();
-        Thread.sleep(2000);
     }
     public void playNextButton() throws InterruptedException{
         WebElement playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
         playNextButton.click();
-        Thread.sleep(2000);
     }
     public void playButton() throws InterruptedException{
-        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        WebElement playButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@data-testid='play-btn']")));
         playButton.click();
-        Thread.sleep(2000);
     }
     public void soundBars(){
-        WebElement usersAvatar = driver.findElement(By.cssSelector("img[alt='Sound bars']"));
+        WebElement usersAvatar = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("img[alt='Sound bars']")));
         Assert.assertTrue(usersAvatar.isDisplayed());
     }
 }
