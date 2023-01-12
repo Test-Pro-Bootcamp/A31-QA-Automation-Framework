d1de88c15daee868c35d38139199ef67ceac8f74
shalinibaronia <50969029+shalinibaronia@users.noreply.github.com>
2023-01-12 08:01:07 -0800

    Cleaned up code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,43 +3,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
 
-
 public class BaseTest {
     public static WebDriver driver = null;
-@BeforeSuite
-static void setupClass(){
 
-    WebDriverManager.chromedriver().setup();
-}
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @BeforeMethod
-    public static void launchBrowser(){
+    public static void launchBrowser() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
     @AfterMethod
-    public static void closeBrowser(){
+    public static void closeBrowser() {
         driver.quit();
     }
 
-    public static void clickSubmit() {
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type = 'submit']"));
-        submitButton.click();
-    }
-
-    public static void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
-        passwordField.sendKeys(password);
+    public static void navigateToPage(String urlAdd) {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
     }
 
     public static void provideEmail(String email) {
@@ -47,10 +39,14 @@ static void setupClass(){
         emailField.sendKeys(email);
     }
 
-    public static void navigateToPage(String urlAdd) {
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
+    public static void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordField.sendKeys(password);
+    }
 
+    public static void clickSubmit() {
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type = 'submit']"));
+        submitButton.click();
     }
 
     public static void searchASong(String songTitle) throws InterruptedException {
@@ -58,20 +54,16 @@ static void setupClass(){
         searchSongField.click();
 
         searchSongField.sendKeys(songTitle);
-
         Thread.sleep(4000);
     }
 
-  public static void viewAllSearchResult() throws InterruptedException {
-      WebElement viewAllButton = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+    public static void viewAllSearchResult() throws InterruptedException {
+        WebElement viewAllButton = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+        Thread.sleep(4000);
 
-
-       Thread.sleep(4000);
         viewAllButton.click();
         Thread.sleep(4000);
-
     }
-
 
     public static void selectSong() throws InterruptedException {
         WebElement Song = driver.findElement(By.cssSelector("section[id='songResultsWrapper'] tr:nth-child(2) td:nth-child(1)"));
@@ -90,7 +82,6 @@ static void setupClass(){
         WebElement selectPlaylist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[@class='playlist'][normalize-space()='Newplaylist']"));
         selectPlaylist.click();
         Thread.sleep(2000);
-
     }
 }
 
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -5,30 +5,28 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
-
-
     @Test
-    public static void addSongToPlaylist () throws InterruptedException {
-
+    public static void addSongToPlaylist() throws InterruptedException {
         navigateToPage("https://bbb.testpro.io/");
 
-       provideEmail("shalinibaronia@gmail.com");
-       providePassword("te$t$tudent");
-       clickSubmit();
+        // Login to the Homepage Koel
+        provideEmail("shalinibaronia@gmail.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
         Thread.sleep(2000);
+
+        // Search and select the song
         searchASong("Take My Hand");
         Thread.sleep(2000);
         viewAllSearchResult();
         selectSong();
+
+        // Add the song to playlist
         clickAddToButton();
         selectPlaylistName();
 
-
-
+        // Verify that song is added to the playlist
         WebElement addedSongAlert = driver.findElement(By.cssSelector("div.success.show"));
         Assert.assertTrue(addedSongAlert.isDisplayed());
-
     }
-
 }
-
