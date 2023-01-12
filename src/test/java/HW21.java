1dd23c0c7ab9da09681073dba47ad70219f5f0a9
TatGur <tatsiana.guryev@testpro.io>
2023-01-11 21:24:43 -0600

    just start
    start

---
+++ src/test/java/HW21.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class HW21 extends BaseTest{
+
+    @Test
+    public void renamePlaylist(){
+        login("tatsianahuryeva@yahoo.com","te$t$tudent");
+        
+
+    }
+}
