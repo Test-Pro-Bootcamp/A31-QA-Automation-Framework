c4d7630a38391ffd99dd113c819b93e5d60f8cc0
shalinibaronia <50969029+shalinibaronia@users.noreply.github.com>
2023-01-12 19:44:41 -0800

    Delete a playlist HW 19

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,58 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
+import org.testng.annotations.Parameters;
+import java.time.Duration;
 
 public class BaseTest {
+    public static WebDriver driver = null;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    @Parameters("BaseUrl")
+    public static void launchBrowser(String BaseUrl) {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get(BaseUrl);
+    }
+
+    @AfterMethod
+    public static void closeBrowser() {
+        driver.quit();
+    }
+
+    protected void login(String email, String password) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type = 'submit']"));
+        submitButton.click();
+    }
+
+    public void selectPlaylist() throws InterruptedException {
+        WebElement openPlaylist = driver.findElement(By.xpath("//a[normalize-space()='Newplaylist']"));
+        openPlaylist.click();
+        Thread.sleep(2000);
+    }
+
+    public void deleteSelectedPlaylist() throws InterruptedException {
+        WebElement deletePlaylistBtn = driver.findElement(By.cssSelector("button[title='Delete this playlist']"));
+        deletePlaylistBtn.click();
+        Thread.sleep(2000);
+    }
 }
---
+++ src/test/java/Homework19.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        launchBrowser("https://bbb.testpro.io/");
+        login("shalinibaronia@gmail.com","te$t$tudent");
+        selectPlaylist();
+        deleteSelectedPlaylist();
+
+        // Verify playlist is deleted alert appears
+        WebElement alertDeleteMsg = driver.findElement(By.xpath(" //div[@class='success show']"));
+        Assert.assertTrue(alertDeleteMsg.isDisplayed());
+    }
+}
