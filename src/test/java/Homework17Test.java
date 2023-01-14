d4d8cd81c9ca2400f792c2c8cb58c01ab8ce298a
JarrodDennis <jarrod.dennis@testpro.io>
2023-01-14 15:34:26 -0500

    Assignment 18 Reworking

--- src/test/java/Homework17Test.java
+++ src/test/java/Homework17Test.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
-
 public class Homework17Test extends BaseTest {
 
     public boolean isNotificationPopUpPresent(){
