2bebc1398ad5abfa8e5c15e50d610c776209e53f
JarrodDennis <jarrod.dennis@testpro.io>
2023-01-14 15:28:07 -0500

    Assignment 17 Reworking

---
+++ src/test/java/HomeWork14Test.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+public class HomeWork14Test extends BaseTest {
+    @Test
+    public static void navigation() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement registrationLink = driver.findElement(By.cssSelector("a[type = submit]"));
+        registrationLink.click();
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+        driver.quit();
+    }
+}
