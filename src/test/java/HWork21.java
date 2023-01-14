2855aa5664e352a31eecac3d437e191c0a9c1858
TatGur <tatsiana.guryev@testpro.io>
2023-01-13 18:35:49 -0600

    Done!

--- src/test/java/HWork21.java
+++ src/test/java/HWork21.java
@@ -3,27 +3,43 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HWork21 extends BaseTest {
-
     @Test
-    public void renamePlaylist(){
-        login("tatsianahuryeva@yahoo.com","te$t$tudent");
+    public void renamePlaylist() {
+        login("tatsianahuryeva@yahoo.com", "te$t$tudent");
+        choosePlaylist();
+        conTextClick();
+        newName();
+        Assert.assertTrue(checkPlayList());
+    }
+    public void choosePlaylist() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+    }
 
-        WebElement element = driver.findElement(By.cssSelector("#playlists > ul > li:nth-child(3) > a"));
+    public void conTextClick() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        WebElement element= driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
         Actions action = new Actions(driver);
-        action.moveToElement(element).perform();
-
-        Actions action1 = new Actions(driver);
-        action1.doubleClick();
-
-        WebElement inputNewName;
-        inputNewName=driver.findElement(By.xpath("//*[@id='playlists']/ul/li[3]/a"));
-        inputNewName.clear();
-        wait.until(ExpectedConditions.visibilityOf(inputNewName));
-        inputNewName.sendKeys("newPlayListName");
-        inputNewName.sendKeys(Keys.ENTER);
+        action.contextClick(element).perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li[data-testid^='playlist-context-menu-edit']"))).click();
+    }
+    public void newName() {
+           WebElement inputNewName = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[name='name']")));
+           inputNewName.sendKeys((Keys.chord(Keys.CONTROL,"a",Keys.BACK_SPACE)));
+           String newPlayListName = "Tatsiana";
+           inputNewName.sendKeys(newPlayListName);
+           inputNewName.sendKeys(Keys.ENTER);
 
     }
-}
+    public boolean checkPlayList (){
+        String newPlayListName = "Tatsiana";
+        WebElement createdPlayList = driver.findElement(By.xpath("//a[text()='"+newPlayListName+"']"));
+         return createdPlayList.isDisplayed();
+    }
+    }
+
+
+
