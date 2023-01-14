19d247a6a6dd3d33cf8145c02a200c01d66014c7
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-13 23:53:33 -0600

    Update LoginPage.java

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -3,31 +3,45 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 
 public class LoginPage extends BasePage {
 
-    By emailFieldLocator = By.cssSelector("[type='email']");
-    By passwordFieldLocator = By.cssSelector("[type='password']");
-    By submitButtonLocator = By.cssSelector("button[type='submit']");
+    WebDriver driver;
+    WebDriver wait;
+
+    //Locators
+    By emailField = By.cssSelector("[type='email']");
+    By passwordField = By.cssSelector("[type='password']");
+    By submitButton = By.cssSelector("button[type='submit']");
 
 
-    public LoginPage(WebDriver givenDriver) { super(givenDriver); }
-
-    public void provideEmail(String email) { driver.findElement(emailFieldLocator).sendKeys(email); }
-
-    @Find(css="[test]")
-    WebElement email;
-
-    public void providePassword(String password) { driver.findElement(passwordFieldLocator).sendKeys(password); }
-
-    public LoginPage clickSubmitBtn(){
-        driver.findElement(submitButtonLocator).click();
-        return this;
+    public LoginPage(WebDriver givenDriver) {
+        driver = givenDriver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
-
+    public void provideEmail(String email) {
+        WebElement emailElement = driver.findElement(emailField);
+        emailElement.click();
+        emailElement.sendKeys(email);
+    }
+    public void providePassword(String password) {
+        WebElement passwordElement = driver.findElement(passwordField);
+        passwordElement.click();
+        passwordElement.sendKeys(password);
+    }
+    public void clickSubmitBtn(){
+        driver.findElement(submitButton).click();
+    }
     public void login(){
         provideEmail("jrpasia@gmail.com");
         providePassword("B3n@iah2013");
         clickSubmitBtn();
     }
+    
     }
