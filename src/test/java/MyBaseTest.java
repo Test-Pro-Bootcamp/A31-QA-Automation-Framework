f687487e868300cfb9aaf8366de9e83d64a1a76c
Simplyd2d <121208284+SimplyDD@users.noreply.github.com>
2023-01-14 15:55:42 -0500

    Homework 17A
    Homework updated BaseTest updated TestNG updated

---
+++ src/test/java/MyBaseTest.java
@@ -0,0 +1,115 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
+
+
+
+public class MyBaseTest {
+    public static WebDriver driver = null;
+    public static String url = "https://bbb.testpro.io/";
+
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public static void launchBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
+    }
+
+    @AfterMethod
+    public static void closeBrowser() {
+        driver.quit();
+    }
+
+
+    protected static void navigateToPage() {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
+
+    public static void login(String email, String password) {
+        enterEmail(email);
+        enterPassword(password);
+        clickSubmit();
+        //Thread.sleep (1000);
+    }
+
+    public static void enterEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys(email);
+
+    }
+
+    public static void enterPassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys(password);
+    }
+
+    public static void clickSubmit() {
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+    }
+
+    public void searchSong(String songTitle) throws InterruptedException {
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type=search]"));
+        searchField.sendKeys(songTitle);
+        searchField.click();
+        Thread.sleep(2000);
+    }
+
+    public static void viewAllSearchResults() throws InterruptedException{
+        WebElement viewAllSearchResults = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn"));
+        viewAllSearchResults.click();
+        Thread.sleep(2000);
+
+        clickAddToButton();
+        Thread.sleep(2000);
+    }
+
+    public static void selectSong() throws InterruptedException {
+        WebElement selectSong = driver.findElement(By.cssSelector("section[id='songResultsWrapper'] tr:nth-child(2) td:nth-child(1)"));
+        selectSong.click();
+        Thread.sleep(2000);
+
+
+    }
+
+    public static void clickAddToButton() throws InterruptedException {
+        WebElement addTo = driver.findElement(By.xpath("//button.btn-add-to"));
+        addTo.click();
+        Thread.sleep(2000);
+
+    }
+
+    public void selectPlaylistname(String playlistName) throws InterruptedException {
+        WebElement selectPlaylist = driver.findElement(By.xpath("//*[@id=\"playlists\"]/ul/li[5]"));
+
+        selectPlaylist.click();
+        Thread.sleep(2000);
+    }
+
+    public String getNotificationText() {
+        WebElement notificationText = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationText.getText();
+    }
+
+    //public boolean isNotificationPopUpPresent() {
+    // notificationText = driver.findElement(By.cssSelector("div.success.show"));
+    //return notificationText.isDisplayed();
+    }
+
