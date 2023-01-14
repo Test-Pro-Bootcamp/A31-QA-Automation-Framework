1af29a8fb2c212c81cdfb4aced7aa55c00e7a042
JarrodDennis <jarrod.dennis@testpro.io>
2023-01-14 15:33:17 -0500

    Assignment 17 Reworking

--- src/test/java/Homework17Test.java
+++ src/test/java/Homework17Test.java
@@ -1,45 +1,55 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertTrue;
 
 public class Homework17Test extends BaseTest {
-    @Test(description = "Grab the Log In Page")
-    public void loginEmptyEmailPasswordTest() {
+
+    public boolean isNotificationPopUpPresent(){
+        WebElement notificationText = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationText.isDisplayed();
+    }
+
+
+    @Test(description = "Move a Song to a Play List")
+    public void addASong() throws InterruptedException {
         grabUrl();
-        String url = grabUrl();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-    }
-    @Test(enabled = false, description = "Log in with Valid User Info")
-     public void loginValidEmailPasswordTest(){
-        logIn("skyeman75@gmail.com", "te$t$tudent");
-
-    }
-    @Test( enabled = false, description = "Invalid User Email")
-    public void loginInvalidEmailPasswordTest(){
-       logIn("Invalid@class.com", "te$t$tudent");
-       grabUrl();
-       String url = grabUrl();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-    }
-    @Test(priority = 1, description = "Move a Song to a Play List")
-    public void addASong(){
-        logIn("skyeman75@gmail.com", "te$t$tudent");
-        WebElement allSongs = driver.findElement(By.cssSelector("a[href='#!/songs']"));
-        allSongs.click();
-        WebElement selectSong = driver.findElement(By.xpath("//tr[@class='song-item selected']"));
-        selectSong.click();
-        WebElement playList = driver.findElement(By.cssSelector("a[href='#!/playlist/35821']"));
-        Actions act = new Actions(driver);
-        act.dragAndDrop(selectSong,playList).build().perform();
-
-
+        logIn("skyeman75@gmail.com","te$t$tudent");
+        driver.manage().window().maximize();
+        searchSong("Pluto");
+        viewAllSearchResults();
+        selectFirstSongResult();
+        clickAddToButton();
+        choosePlaylist("Test Pro Playlist");
+        Assert.assertTrue(isNotificationPopUpPresent());
     }
 
-
-
-
-
+    public void searchSong(String songTitleKeyword)throws InterruptedException{
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type='search']"));
+        searchField.sendKeys(songTitleKeyword);
+        Thread.sleep(2000);
+    }
+    public void viewAllSearchResults()throws InterruptedException {
+        WebElement viewAllSearchResult = driver.findElement(By.cssSelector("div.results section.songs h1 button"));
+        viewAllSearchResult.click();
+        Thread.sleep(2000);
+    }
+    public void selectFirstSongResult()throws InterruptedException {
+        WebElement viewAllFirstSongResult = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
+        viewAllFirstSongResult.click();
+        Thread.sleep(2000);
+    }
+    public void clickAddToButton()throws InterruptedException{
+        WebElement addTo = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addTo.click();
+        Thread.sleep(2000);
+    }
+    public void choosePlaylist(String playlistName)throws InterruptedException{
+        WebElement playListNameElement  = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//section/ul/li[contains(text(),'"+playlistName+"')]"));
+        playListNameElement.click();
+        Thread.sleep(2000);
+    }
 }
+
