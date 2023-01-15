3126fe29dc96c44d33dbb9fd6fcb4b05d99e8377
sumankarki01 <suman.karki@testpro.io>
2023-01-15 00:54:16 -0800

    done

--- src/test/java/Home19work.java
+++ src/test/java/Home19work.java
@@ -7,32 +7,40 @@ import org.testng.annotations.Test;
 import javax.naming.Name;
 import java.util.UUID;
 
-public class Home19work extends BaseTest{
+public class Home19work extends BaseTest {
     @Test
-    public void deletePlaylist(){
-        login("karkisuman0330@gmail.com","te$t$tudent");
-
+    public void deletePlaylist() throws InterruptedException {
+        login("karkisuman0330@gmail.com", "te$t$tudent");
         newPlaylist();
-        chknewPlaylist();
         dltplaylist();
+        dltPlaylist1();
+
+        WebElement dltMESS = popUPNot();
+       Assert.assertFalse(dltMESS.isDisplayed());
 
     }
 
-    public void  newPlaylist() {
+    public void  newPlaylist() throws InterruptedException {
+       Thread.sleep(3000);
         driver.findElement(By.cssSelector("[data-testid='sidebar-create-playlist-btn']")).click();
 
         driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-simple']")).click();
 
         WebElement nameList = driver.findElement(By.cssSelector("input[name='name']"));
 
-        nameList.sendKeys("rocksg" + Keys.ENTER);
-    }
-  public void chknewPlaylist(){
-        WebElement rockSngPlaylist =driver.findElement(By.xpath("//*[@id='playlists']/ul/li[3]/a)"));
-        Assert.assertEquals(rockSngPlaylist,"rocksg");
-    }
-    public void dltplaylist(){
-
+        nameList.sendKeys("rocksg",Keys.ENTER);
     }
 
+    public void dltplaylist() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//li/a[text()='rocksg']")).click();
+        }
+   public void dltPlaylist1() throws InterruptedException {
+       Thread.sleep(5000);
+        driver.findElement(By.cssSelector(".btn-delete-playlist")).click();
+
+    }
+   public WebElement popUPNot(){
+        return driver.findElement(By.cssSelector("div.success.show"));
+    }
 }
