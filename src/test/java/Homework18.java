f65126d457cd3b2280afefe7436e22e6c9590f5f
szandragecse <szandra.gecse@testpro.io>
2023-01-15 23:29:17 -0500

    Homework#20

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -5,24 +5,24 @@ import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
 
-    @Test
-    public void playASongTest() {
-        login("demo@class.com", "te$t$tudent");
-        playSong();
-        isSongPlaying();
-    }
-
-    public void playSong() {
-        WebElement playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
-        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
-
-        playNextButton.click();
-        playButton.click();
-    }
-
-    public void isSongPlaying() {
-        WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
-        Assert.assertTrue(soundBar.isDisplayed());
-    }
+//    @Test
+//    public void playASongTest() {
+//        login("demo@class.com", "te$t$tudent");
+//        playSong();
+//        isSongPlaying();
+//    }
+//
+//    public void playSong() {
+//        WebElement playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+//        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+//
+//        playNextButton.click();
+//        playButton.click();
+//    }
+//
+//    public void isSongPlaying() {
+//        WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+//        Assert.assertTrue(soundBar.isDisplayed());
+//    }
 
 }
---
+++ src/test/java/Homework21.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest{
+
+    @Test
+    public static void renamePlaylist() throws InterruptedException {
+        login();
+
+
+    }
+}
