ed10359ae51f53dffa1b5cd033f62b1ed9ddb3d5
AlexPaulavets <121207814+AlexPaulavets@users.noreply.github.com>
2023-01-16 17:42:45 -0600

    Tried to fix 'Action' error

--- src/test/java/SeleniumTechniques.java
+++ src/test/java/SeleniumTechniques.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -8,9 +9,10 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import org.openqa.selenium.interactions.Interaction;
 
 public class SeleniumTechniques extends BaseTest {
-
+    Actions action = new Actions(driver);
 
     //      context click (sometimes koel page does not load completely, re-run the test)
     @Test
