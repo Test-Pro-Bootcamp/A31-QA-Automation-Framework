544d337c6cc4fdd63444cafb40f4fba54f0d0f95
GreeshmaUdupa <121182336+GreeshmaUdupa@users.noreply.github.com>
2023-01-19 10:50:10 -0600

    Added Homework23 file

---
+++ src/test/java/Homework23.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class Homework23 extends BaseTest {
+    @Test
+    public void renamePlaylist() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.login("test123@test.com", "te$t$tudent");
+        homePage.createPlaylist("HW");
+        homePage.choosePlaylist("HW");
+        homePage.doubleClickChoosePlaylist("HW");
+        homePage.enterPlaylistName("HW22");
+        Assert.assertTrue((homePage.getSuccessMsg()));
+    }
+
+}
