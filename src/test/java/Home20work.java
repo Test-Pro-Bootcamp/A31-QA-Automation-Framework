c8cfbcf4662268d3e16f20aad20a267d8fce9c3f
sumankarki01 <suman.karki@testpro.io>
2023-01-20 15:32:05 -0800

    wait

--- src/test/java/Home20work.java
+++ src/test/java/Home20work.java
@@ -1,30 +1,38 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 
 public class Home20work {
-    @Test
-    public void clearCurrentQueue() {
-        starup();
-        //login();
-    }
+    static WebDriverWait wait=null;
 
-    private void starup() {
+    @Test
+    private static void registration1() {
+
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10))
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.clear();
-        emailField.sendKeys("karkisuman0330@gmail.com");
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.clear();
-        passwordField.sendKeys("te$t$tudent");
+
+        WebElement registrationButton1 = wait.until(ExpectedConditions.elementToBeClickable(By.id("[id='hel']")));
+        //driver.findElement(By.id("[id='hel']"));
+        registrationButton1.click();
+
+
+        WebElement registerButton1 = driver.findElement(By.id("[id='button']"));
+        Assert.assertTrue(registerButton1.isDisplayed());
+
+        driver.quit();
     }
 
 }
