e46967205397ffcba131fe78b133a7edb9017b75
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-20 16:16:48 -0600

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,19 +1,24 @@
-package pages;
+package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage extends BasePage {
+import java.time.Duration;
 
-    By userAvatarIcon = By.cssSelector("img.avatar");
+public class HomePage extends Pages.BasePage {
+
+    private By userAvatarIcon = By.cssSelector("img.avatar");
 
     public HomePage( WebDriver givenDriver) {
         super(givenDriver);
+        driver = givenDriver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
-
     public WebElement getUserAvatar () {
-        return findElement(userAvatarIcon);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
     }
 
 }
