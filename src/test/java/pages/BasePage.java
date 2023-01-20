88645bc4ac6f8f6f2678844a8377bc995eec8677
YingYangA31 <ying.yang@testpro.io>
2023-01-20 15:42:03 -0500

    Refractor code with implement Page Factory and Fluent Interfaces.

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,6 +21,7 @@ public class BasePage {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         actions = new Actions(driver);
+        PageFactory.initElements(driver,this);
     }
 
     public void click(By locator) {
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,35 +3,35 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
 
+    //WebElements
+    @FindBy(css = "[type='submit']")
+    WebElement submitButtonLocator;
+    @FindBy(css = "[type='email']")
+    WebElement emailField;
+    @FindBy(css = "[type='password']")
+    WebElement passwordField;
+
     //locators
-    By submitButtonLocator = By.cssSelector("[type='submit']");
-    By emailField = By.cssSelector("[type='email']");
-    By passwordField = By.cssSelector("[type='password']");
+    //By submitButtonLocator = By.cssSelector("[type='submit']");
+    //By emailField = By.cssSelector("[type='email']");
+    //By passwordField = By.cssSelector("[type='password']");
 
     public LoginPage( WebDriver givenDriver) {
         super(givenDriver);
     }
-
-
-
-    public void clickSubmitBtn() {
-        driver.findElement(submitButtonLocator).click();
+    public LoginPage clickSubmitBtn() { (submitButtonLocator).click();
+        return this;
     }
-
-    public void provideEmail(String email) {
-        WebElement emailElement = driver.findElement(emailField);
-        emailElement.sendKeys(email);
+    public LoginPage provideEmail(String email) { emailField.sendKeys(email);
+        return this;
     }
-
-    public void providePassword(String password) {
-        WebElement passwordElement = driver.findElement(passwordField);
-        passwordElement.sendKeys(password);
-
+    public LoginPage providePassword(String password) { passwordField.sendKeys(password);
+        return this;
     }
-
     public void login(){
         provideEmail("dummytest@class.com");
         providePassword("te$t$tudent");
