478fb78c4d612a6c39c1a9efd5011e215f88cf09
PGgit1 <121201098+PGgit1@users.noreply.github.com>
2023-01-19 17:39:42 -0800

    PG-Homework23
    Refactor to @findby

--- src/test/java/pages/AllSongsPage.java
+++ src/test/java/pages/AllSongsPage.java
@@ -3,31 +3,37 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AllSongsPage extends BasePage {
 
-    By allSongsLocator = By.cssSelector("a[class='songs']");
+    @FindBy (css = "a[class='songs']")
+            WebElement allSongsLocator;
 
-    By songNameLocator = By.cssSelector("#songsWrapper > div > div > div.item-container > table > tr:nth-child(1) > td.title");
+    @FindBy (xpath = "#songsWrapper > div > div > div.item-container > table > tr:nth-child(1) > td.title")
+            WebElement songNameLocator;
 
-    By playNextButtonLocator = By.xpath("//i[@data-testid='play-next-btn']");
+    @FindBy (xpath = "//i[@data-testid='play-next-btn']")
+            WebElement playNextButtonLocator;
 
-    By playButtonLocator = By.xpath("//span[@data-testid='play-btn']");
+    @FindBy (xpath = "//span[@data-testid='play-btn']")
+            WebElement playButtonLocator;
 
-    By soundBarsLocator = By.cssSelector("img[alt='Sound bars']");
+    @FindBy (css = "img[alt='Sound bars']")
+            WebElement soundBarsLocator;
 
     public AllSongsPage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
-    public void allSongs(){ driver.findElement(allSongsLocator).click();}
+    public void allSongs(){allSongsLocator.click();}
 
-    public void songName(){ driver.findElement(songNameLocator).click();}
+    public void songName(){songNameLocator.click();}
 
-    public void playNextButton(){ driver.findElement(playNextButtonLocator).click();}
+    public void playNextButton(){playNextButtonLocator.click();}
 
-    public void playButton(){ driver.findElement(playButtonLocator).click();}
+    public void playButton(){playButtonLocator.click();}
 
     public boolean soundBars(){
         WebElement userAvatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[alt='Sound bars']")));
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,6 +19,7 @@ public class BasePage {
     public BasePage( WebDriver givenDriver) {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        PageFactory.initElements(driver, this);
     }
 
     public void click(By locator) {
