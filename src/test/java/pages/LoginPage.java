d4fdbd24790d7ec99f042b03e6c68c32d7dcfd20
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-20 16:17:24 -0600

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,38 +1,40 @@
-package pages;
+package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends Pages.BasePage {
 
-    //locators
-    By submitButtonLocator = By.cssSelector("[type='submit']");
-    By emailField = By.cssSelector("[type='email']");
-    By passwordField = By.cssSelector("[type='password']");
+    //Locators
+    private By emailField = By.cssSelector("[type='email']");
+    private By passwordField = By.cssSelector("[type='password']");
+    private By submitButton = By.cssSelector("button[type='submit']");
 
-    public LoginPage( WebDriver givenDriver) {
+    @FindBy(css = "[test]")
+    WebElement email;
+
+    public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
-    }
-
-    public void clickSubmitBtn() {
-        driver.findElement(submitButtonLocator).click();
     }
 
     public void provideEmail(String email) {
         WebElement emailElement = driver.findElement(emailField);
+        emailElement.click();
         emailElement.sendKeys(email);
     }
-
     public void providePassword(String password) {
         WebElement passwordElement = driver.findElement(passwordField);
+        passwordElement.click();
         passwordElement.sendKeys(password);
-
     }
-
-    public void logIn(){
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
+    public void clickSubmitBtn(){
+        driver.findElement(submitButton).click();
+    }
+    public void login(){
+        provideEmail("jrpasia@gmail.com");
+        providePassword("B3n@iah2013");
         clickSubmitBtn();
     }
 
