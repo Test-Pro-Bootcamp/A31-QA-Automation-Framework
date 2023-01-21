2ab6d5f27b1df7ca57cf8f3635d41da9ffb9c233
sumankarki01 <suman.karki@testpro.io>
2023-01-20 16:31:46 -0800

    done

---
+++ src/test/java/Homework21.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest {
+login("karkisuman0330@gmail.com","te$t$tudent");
+WebElement wait=null;
+@Test
+
+
+}
