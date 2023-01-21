3519ecee34872f9bfbf55618dbcabcef52f55ccb
AlexGuyvan <guyvanalex4@gmail.com>
2023-01-20 18:07:59 -0600

    AG_Homework(Page Factory)

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,6 +20,7 @@ public class BasePage {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public void click(By locator) {
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,30 +3,35 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
 
     //locators
-    By submitButtonLocator = By.cssSelector("[type='submit']");
-    By emailField = By.cssSelector("[type='email']");
-    By passwordField = By.cssSelector("[type='password']");
+    @FindBy(css= "[type='submit']")
+            WebElement submitButtonLocator;
+//    By submitButtonLocator = By.cssSelector("[type='submit']");
+    @FindBy(css="[type='email']")
+        WebElement emailField;
+//    By emailField = By.cssSelector("[type='email']");
+    @FindBy(css="[type='password']")
+        WebElement passwordField;
+//    By passwordField = By.cssSelector("[type='password']");
 
     public LoginPage( WebDriver givenDriver) {
         super(givenDriver);
     }
 
     public void clickSubmitBtn() {
-        driver.findElement(submitButtonLocator).click();
+        submitButtonLocator.click();
     }
 
     public void provideEmail(String email) {
-        WebElement emailElement = driver.findElement(emailField);
-        emailElement.sendKeys(email);
+        emailField.sendKeys(email);
     }
 
     public void providePassword(String password) {
-        WebElement passwordElement = driver.findElement(passwordField);
-        passwordElement.sendKeys(password);
+        passwordField.sendKeys(password);
 
     }
 
