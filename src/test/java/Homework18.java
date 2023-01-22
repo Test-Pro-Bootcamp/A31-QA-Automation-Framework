66f8e85cf6f4feddc7a8ff850fd20d2aa8bd9bf1
Simplyd2d <121208284+SimplyDD@users.noreply.github.com>
2023-01-22 18:41:39 -0500

    Homework 18
    Playsong 18 Homework

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -6,18 +6,22 @@ import org.testng.annotations.Test;
 public class Homework18 extends BaseTest {
 
     @Test
-    public void playASongTest() {
-        login("demo@class.com", "te$t$tudent");
+    public void playASongTest() throws InterruptedException{
+        login("simplyd2d@gmail.com", "te$t$tudent");
+        Thread.sleep(3000);
         playSong();
         isSongPlaying();
     }
 
-    public void playSong() {
+    public void playSong() throws InterruptedException{
+
         WebElement playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
         WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
 
         playNextButton.click();
         playButton.click();
+        Thread.sleep(5000);
+
     }
 
     public void isSongPlaying() {
