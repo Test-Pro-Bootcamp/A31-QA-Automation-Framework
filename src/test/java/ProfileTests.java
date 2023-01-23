9a2234025d4a76d26e3b74da430781fd4fdd82d4
khaledoni01 <khaledoni01@gmail.com>
2023-01-23 13:32:32 -0500

    selenium-parallel

--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -2,11 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 
 public class ProfileTests extends BaseTest {
 
     @Test(enabled = true, priority = 3, description = "update profile test")
-    public static void updateProfileNameTest () throws InterruptedException {
+    public void updateProfileNameTest () throws InterruptedException {
+        LoginPage loginPage = new LoginPage(getDriver());
 
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
