52ae161b8c9a80a06955ac63a449f38294927102
JarrodDennis <jarrod.dennis@testpro.io>
2023-01-23 16:24:03 -0500

    Assignment 22 POM Redone......Not sure what I am doing wrong. I'll check the homework review in the next chapter

--- src/test/java/Homework21Test.java
+++ src/test/java/Homework21Test.java
@@ -1,7 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePageTest;
