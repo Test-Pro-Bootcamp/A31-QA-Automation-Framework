826543cd57be129fa9aaa965e445cca6754fd309
Simplyd2d <121208284+SimplyDD@users.noreply.github.com>
2023-01-22 19:16:47 -0500

    Homework19
    Delete empty playlist

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest {
+
+    @Test
+    public void deleteEmptyPlaylist() throws InterruptedException {
+        login("simplyd2d@gmail.com", "te$t$tudent");
+        clickSubmit();
+
+        //select playlist Homework 19 Test
+        WebElement selectPlaylist = driver.findElement(By.xpath("//a[normalize-space()='Homework 19 Test']"));
+        selectPlaylist.click();
+
+        // Delete selected playlist
+        WebElement deletePlaylist = driver.findElement(By.xpath("//button[normalize-space()='Playlist']"));
+        deletePlaylist.click();
+        Thread.sleep(3000);
+
+        // Validate that the playlist is deleted
+        WebElement deleteMsg = driver.findElement(By.xpath(" //div[@class='success show']"));
+        Assert.assertTrue(deleteMsg.isDisplayed());
+        Thread.sleep(3000);
+
+    }
+
+
+    }
+
+
