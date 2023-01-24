e0233713908f36835c466a2789e099edfa02ed69
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-23 19:10:42 -0600

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,18 +3,36 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class HomePage extends BasePage {
+public class HomePage extends pages.BasePage {
 
-    By userAvatarIcon = By.cssSelector("img.avatar");
+    //Locators
+    @FindBy(css = "[class='home active']")
+    private WebElement homeButton;
+    @FindBy(css = "[class='queue']")
+    private WebElement currentQue;
+    @FindBy(css = "[class='songs']")
+    private WebElement allSongsButton;
+    private By userAvatarIcon = By.cssSelector("img.avatar");
 
     public HomePage( WebDriver givenDriver) {
-
         super(givenDriver);
     }
-
     public WebElement getUserAvatar () {
-        return findElement(userAvatarIcon);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
     }
-
+    public HomePage clickHomeButton() {
+        homeButton.click();
+        return this;
+    }
+    public HomePage clickCurrentQueBtn() {
+        currentQue.click();
+        return this;
+    }
+    public HomePage clickAllSongsBtn() {
+        allSongsButton.click();
+        return this;
+    }
 }
