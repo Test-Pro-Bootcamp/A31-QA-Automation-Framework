e6791e3effa90264ae13417c91bb73e62dc3946b
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-24 14:38:12 -0600

    Update

---
+++ src/test/java/CurrentQueTests.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class CurrentQueTests extends BaseTest {
+
+    @Test
+    public void addASongTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        BasePage basePage = new BasePage(driver);
+        Actions acts = new Actions(driver);
+
+        //Login valid credentials
+        loginPage.login();
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+
+        //Click All Songs link
+        homePage.clickAllSongsBtn();
+
+        //Click "Ketsa" song
+        basePage.contextClick();
+
+        //Click "Add To"
+        basePage.findElement(By.xpath("//ul/li[@class='has-sub']")).click();
+
+        //Click "Top of Queue"
+        basePage.findElement(By.xpath("//ul/li[@class='has-sub']/ul/li[3]")).click();
+
+        //Assert song is added to que
+        homePage.clickCurrentQueBtn();
+        basePage.findElement(By.xpath("//*[@id=\"queueWrapper\"]/div/div/div[1]/table/tr/td[2]"));
+
+
+
+
+    }
+}
---
+++ src/test/java/pages/AllSongsPage.java
@@ -0,0 +1,6 @@
+package pages;
+
+public class AllSongsPage {
+
+
+}
