6f4db7e10f1338d93ef878d877ee9c7ade36cee0
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-23 19:24:49 -0600

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,36 +3,50 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
-public class HomePage extends pages.BasePage {
+import java.time.Duration;
 
-    //Locators
-    @FindBy(css = "[class='home active']")
-    WebElement homeButton;
-    @FindBy(css = "[class='queue']")
-    WebElement currentQue;
-    @FindBy(css = "[class='songs']")
-    WebElement allSongsButton;
-    private By userAvatarIcon = By.cssSelector("img.avatar");
+public class BasePage {
 
-    public HomePage( WebDriver givenDriver) {
-        super(givenDriver);
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions actions;
+
+    public BasePage( WebDriver givenDriver) {
+        driver = givenDriver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        PageFactory.initElements(driver, this);
     }
-    public WebElement getUserAvatar () {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
-    public HomePage clickHomeButton() {
-        homeButton.click();
-        return this;
+
+    public WebElement findElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
-    public HomePage clickCurrentQueBtn() {
-        currentQue.click();
-        return this;
+
+    public void contextClick(By locator) {
+        actions.contextClick(findElement(locator)).perform();
     }
-    public HomePage clickAllSongsBtn() {
-        allSongsButton.click();
-        return this;
+    public void doubleClick (By locator) {
+        actions.doubleClick(findElement(locator)).perform();
     }
+    public void assertAddDeleteMessage() {
+        WebElement addDeleteMessageBox = driver.findElement(By.cssSelector("div[class='alertify-logs top right']"));
+        Assert.assertTrue(addDeleteMessageBox.isDisplayed());
+    }
+    public void clickPlusIcon() {
+        WebElement plusIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//i[@title='Create a new playlist']")));
+        plusIcon.click();
+    }
+    public void clickNewPlaylist() {
+        WebElement newPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[text()='New Playlist']")));
+        newPlaylist.click();
+    }
+
 }
