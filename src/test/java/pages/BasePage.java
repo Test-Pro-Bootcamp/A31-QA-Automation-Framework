2f932e906fa72c507bbd42b4d5bfe089b904edb2
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-01-23 19:09:56 -0600

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,37 +3,36 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+public class HomePage extends pages.BasePage {
 
-public class BasePage {
+    //Locators
+    @FindBy(css = "[class='home active']")
+    private WebElement homeButton;
+    @FindBy(css = "[class='queue']")
+    private WebElement currentQue;
+    @FindBy(css = "[class='songs']")
+    private WebElement allSongsButton;
+    private By userAvatarIcon = By.cssSelector("img.avatar");
 
-    WebDriver driver;
-    WebDriverWait wait;
-    Actions actions;
-
-    public BasePage( WebDriver givenDriver) {
-        driver = givenDriver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    public HomePage( WebDriver givenDriver) {
+        super(givenDriver);
     }
-
-    public void click(By locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    public WebElement getUserAvatar () {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
     }
-
-    public WebElement findElement(By locator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    public HomePage clickHomeButton() {
+        homeButton.click();
+        return this;
     }
-
-    public void contextClick(By locator) {
-        actions.contextClick(findElement(locator)).perform();
+    public HomePage clickCurrentQueBtn() {
+        currentQue.click();
+        return this;
     }
-
-    public void doubleClick (By locator) {
-        actions.doubleClick(findElement(locator)).perform();
+    public HomePage clickAllSongsBtn() {
+        allSongsButton.click();
+        return this;
     }
-
 }
