842608c7cd0b45fe2b4d204c7b7b675a3a3bd770
zahin.hossain <zahin.hossain@testpro.io>
2023-01-25 02:04:38 -0500

    Update BasePage.java

--- src/test/java/PageFactory/BasePage.java
+++ src/test/java/PageFactory/BasePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 
 public class BasePage {
 
