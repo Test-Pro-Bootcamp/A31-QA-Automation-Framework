8ff70c5bc773fa876ca0335f9d8197401a09238b
Rinku <paramvir.singh@testpro.io>
2023-01-26 13:15:43 -0500

    Hw 24
    Implemented setup for selenium grid and different browsers that can be picked through cmd prompt

--- src/test/java/tests/KoelTesting.java
+++ src/test/java/tests/KoelTesting.java
@@ -1,7 +1,11 @@
 package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -10,6 +14,9 @@ import pages.AllSongsPage;
 import pages.BasePage;
 import pages.HomePage;
 import pages.LoginPage;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 public class KoelTesting {
 
@@ -20,15 +27,14 @@ public class KoelTesting {
     public WebDriver driver;
 
     @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.firefoxdriver().setup();
+    public void setupClass() throws MalformedURLException {
+        driver = pickBrowser(System.getProperty("browser"));
     }
 
     @BeforeMethod
     @Parameters({"baseURL", "loginEmail", "loginPassword"})
     public void beforeMethod(String baseURL, String loginEmail, String loginPassword)
     {
-        driver = new FirefoxDriver();
         basePage = new BasePage(driver);
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
@@ -49,6 +55,39 @@ public class KoelTesting {
         driver.manage().window().maximize();
         driver.get(url);
     }
+
+    private WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridUrl = "http://192.168.1.13:4444";
+
+        switch (browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), caps);
+                break;
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), caps);
+                break;
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), caps);
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
 
     public void logIntoKoel(String userName, String password)
     {
