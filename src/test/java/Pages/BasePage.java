2bcdfdfd982a57872656a15d965b9c68dd87b88e
shalinibaronia <50969029+shalinibaronia@users.noreply.github.com>
2023-02-02 14:03:26 -0800

    update

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -9,9 +9,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class BasePage {
-    protected WebDriver driver;
-    protected WebDriverWait wait;
-    protected Actions action;
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions action;
     public BasePage(WebDriver givenDriver){
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
