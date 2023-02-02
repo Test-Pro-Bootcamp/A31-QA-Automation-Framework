2d45c2cea8346cd192f46aabf41843376750a2ca
shalinibaronia <50969029+shalinibaronia@users.noreply.github.com>
2023-02-02 14:43:04 -0800

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,8 +5,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 public class LoginTests extends BaseTest {
-    LoginPage loginPage = new LoginPage(getDriver());
-    HomePage homepage = new HomePage(getDriver());
+   
     @Test
     public void LoginValidEmailPasswordTest () {
         LoginPage loginPage = new LoginPage(getDriver());
