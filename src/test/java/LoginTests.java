140f0a793d3bf7b1546651d0e0dfc2aae2b4d26e
sumankarki01 <suman.karki@testpro.io>
2023-02-02 23:18:06 -0800

    done

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,23 +2,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 
 public class LoginTests extends BaseTest {
-
     //    @Test(enabled = true, priority = 0, description = "LoginEmptyEmailPasswordTest")
-    @Test(dataProvider = "incorrectLoginProviders", dataProviderClass = BaseTest.class)
-    public static void loginEmptyEmailPasswordTest (String email, String password) {
+   // @Test(dataProvider = "incorrectLoginProviders", dataProviderClass = BaseTest.class)
+   @Test
+    public static void loginEmptyEmailPasswordTest () {
+       LoginPage loginPage = new LoginPage(driver);
 
-        login(email, password);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+       loginPage.submitBtn();
+
+       Assert.assertEquals(driver.getCurrentUrl(), url);
     }
 
     @Test(enabled = true, priority = 1, description = "LoginValidEmailValidPasswordTest")
     public static void loginValidEmailValidPasswordTest () throws InterruptedException {
+        LoginPage loginpage = new LoginPage(driver);
+          loginpage.provideEmail("karkisuman0330@gmail.com");
+          loginpage.providePassword("te$t$tudent");
+          loginpage.submitBtn();
 
-        provideEmail("demo@class.com");
+       /* provideEmail("demo@class.com");
         providePassword("te$t$tudent");
-        clickSubmit();
+        clickSubmit();*/
 
         Thread.sleep(2000);
         WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
@@ -28,10 +35,10 @@ public class LoginTests extends BaseTest {
 
     @Test(enabled = true, priority = 2, description = "LoginInvalidEmailValidPasswordTest")
     public static void loginInvalidEmailValidPasswordTest () {
-
-        provideEmail("invalid@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
+       LoginPage loginpage = new LoginPage(driver);
+        loginpage.provideEmail("invalid@class.com");
+       loginpage.providePassword("te$t$tudent");
+       loginpage.submitBtn();
 
         Assert.assertEquals(driver.getCurrentUrl(), url);
 
