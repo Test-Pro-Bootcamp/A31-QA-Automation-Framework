692635658ee3db454a6455c09d925d9b819f9488
rijoal <114624987+rijoal@users.noreply.github.com>
2023-02-03 12:58:23 -0800

    Catching up

---
+++ src/test/java/Homework14.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework14 extends BaseTest {
+
+    @Test
+    public static void Navigation() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement loginField = driver.findElement(By.cssSelector("[type='submit'"));
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password'"));
+        WebElement avatar = driver.findElement(By.className("avatar"));
+
+
+    }
+
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -55,6 +55,9 @@ public class LoginTests extends BaseTest {
         // click into email field
         // enter invalid email inside email field
         WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+
         emailField.click();
         emailField.sendKeys("cucaracha@class.com");
 
@@ -62,15 +65,13 @@ public class LoginTests extends BaseTest {
         // find password field
         // click into password field
         // enter anything
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
         passwordField.click();
         passwordField.sendKeys("ayCaramba");
 
         //Step4. Click Login button
         // find the login button
         // click the login button
-        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
-        loginButton.click();
+       loginButton.click();
 
         //Expected result: User should stay on login page
         // assert the present of email field
@@ -80,12 +81,6 @@ public class LoginTests extends BaseTest {
 
     @Test
     public static void LoginValidEmailPasswordTest() {
-        // Precondition: Chrome browser should be opened DONE
-        //Step1. Open koel login page DONE
-        //Step2. Enter Existing username
-        //Step3. Enter Correct password
-        //Step4. Click Login button
-        //Expected result: User should be directed to the home page
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -93,20 +88,17 @@ public class LoginTests extends BaseTest {
         driver.get(url);
 
         WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement loginField = driver.findElement(By.cssSelector("[type='submit'"));
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password'"));
+        WebElement avatar = driver.findElement(By.className("avatar"));
+
+
+
         emailField.click();
         emailField.sendKeys("demo@class.com");
-
-        //Step3. Enter Correct password
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password'"));
         passwordField.click();
         passwordField.sendKeys("te$t$tudent");
-
-        //Step4. Click Login button
-        WebElement loginField = driver.findElement(By.cssSelector("[type='submit'"));
         loginField.click();
-
-        //Expected result: User should be directed to the home page
-        WebElement avatar = driver.findElement(By.className("avatar"));
         Assert.assertTrue(avatar.isDisplayed());
 
         driver.quit();
