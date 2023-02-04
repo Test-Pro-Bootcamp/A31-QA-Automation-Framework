e8b0f8a096dd7b421ca34b5afbd1bd4fe968623e
TatGur <tatsiana.guryev@testpro.io>
2023-02-03 19:14:35 -0600

    1 commit

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,8 +19,8 @@ public class BasePage {
     public BasePage( WebDriver givenDriver) {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        PageFactory.initElements(driver,this);
     }
-
     public void click(By locator) {
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
