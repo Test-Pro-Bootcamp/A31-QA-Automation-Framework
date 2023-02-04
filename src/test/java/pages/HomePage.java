1abaa5826717d3c7a097a7be26c7bffc1dc0b9b2
TatGur <tatsiana.guryev@testpro.io>
2023-02-04 17:25:03 -0600

    23 HW Page Factory

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,20 +1,46 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage {
 
-    By userAvatarIcon = By.cssSelector("img.avatar");
+    @FindBy(css = "img.avatar")
+    WebElement userAvatarIcon;
 
-    public HomePage( WebDriver givenDriver) {
-
+    public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
-    public WebElement getUserAvatar () {
-        return findElement(userAvatarIcon);
+    public WebElement getUserAvatar() {
+        return userAvatarIcon;
+    }
+    public void choosePlaylist() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+    }
+    public void conTextClick() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        WebElement element = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        Actions action = new Actions(driver);
+        action.contextClick(element).perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li[data-testid^='playlist-context-menu-edit']"))).click();
+    }
+    public void newName() {
+        WebElement inputNewName = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[name='name']")));
+        inputNewName.sendKeys((Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE)));
+        String newPlayListName = "Tatsiana";
+        inputNewName.sendKeys(newPlayListName);
+        inputNewName.sendKeys(Keys.ENTER);
+    }
+    public boolean checkPlayList() {
+        String newPlayListName = "Tatsiana";
+        WebElement createdPlayList = driver.findElement(By.xpath("//a[text()='" + newPlayListName + "']"));
+        return createdPlayList.isDisplayed();
     }
 
 }
