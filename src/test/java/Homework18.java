e2da967784c7c50ab478360990314aa414517970
nicoleliano <nicole.liano@testpro.io>
2023-02-05 15:50:52 -0500

    Homework 18 - added assertion

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
@@ -13,6 +14,11 @@ public class Homework18 extends BaseTest {
         Actions actions = new Actions(driver);
         WebElement doubleClickSong = driver.findElement(By.xpath("//span[contains(text(), 'Mid-Air Machine')]"));
         actions.doubleClick(doubleClickSong).perform();
+
+        Thread.sleep(2000);
+
+        WebElement visualizer = driver.findElement(By.cssSelector("div>img[alt='Sound bars']"));
+        Assert.assertTrue(visualizer.isDisplayed());
 
         closeBrowser();
     }
