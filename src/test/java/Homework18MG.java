4efa48cd406ccf0e36316550f4fba6ffb1f8fe6b
MariiaGrishchenko <mariia.grishchenko@testpro.io>
2023-02-06 00:31:41 -0800

    Update Homework18MG.java
    Second try

--- src/test/java/Homework18MG.java
+++ src/test/java/Homework18MG.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,20 +10,45 @@ public class Homework18MG extends BaseTest{
 
     @Test
     public void playSongTest() {
-        login("maria868g@gmail.com", "Grishchenko_000");
-        playSong();
+        login("demo@class.com", "te$t$tudent");
+        //login("maria868g@gmail.com", "Grishchenko_000");
+        clickAllSongsButton();
+        selectSong();
+        clickButtonPlaySong();
+        Assert.assertTrue(playingSongIsDesplayed());
 
     }
-    public void playSong(){
-        WebElement playButton = driver.findElement(By.cssSelector("[title='Play or resume']"));
-        playButton.click();
+    public void clickAllSongsButton() {
+        WebElement allSongsButton = driver.findElement(By.cssSelector("a[href='#!/songs']"));
+        allSongsButton.click();
+    }
+
+    public void selectSong() {
+        WebElement selectedSong = driver.findElement(By.xpath("//*[contains(text(),'Paean')]"));
+        selectedSong.click();
+    }
+    public void clickButtonPlaySong() {
+        WebElement buttonPlaySong = driver.findElement(By.xpath("//span[@title='Play or resume']"));
+        Actions actions = new Actions(driver);
+        actions.click(buttonPlaySong).perform();
+    }
+    public boolean playingSongIsDesplayed(){
+        WebElement songIsPlaing = driver.findElement(By.cssSelector("[data-testid = 'sound-bar-play']"));
+        return songIsPlaing.isDisplayed();
+    }
+    //public void playSong(){
+       // WebElement playButton = driver.findElement(By.cssSelector("[title='Play or resume']"));
+        //playButton.click();
         //Current song is playing
 
-        WebElement nextSongButton = driver.findElement(By.cssSelector("[title='Play next song']"));
-        nextSongButton.click();
+       // WebElement nextSongButton = driver.findElement(By.cssSelector("[title='Play next song']"));
+       // nextSongButton.click();
         //Next song in queue is playing
 
-        WebElement soundVisualizer = driver.findElement(By.cssSelector("[alt='Sound bars'']"));
-        Assert.assertTrue(soundVisualizer.isDisplayed());
+        //WebElement soundVisualizer = driver.findElement(By.cssSelector("[alt='Sound bars'']"));
+
+        //public boolean playinSongIsDisplayed() {
+            //WebElement songIsPlaing = driver.findElement(By.cssSelector("[data-testid = 'sound-bar-play']"));
+            //Assert.assertTrue(songIsPlaing.isDisplayed());
+        //}
     }
-}
