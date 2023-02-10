c1c5fe8e57f16baccd2a9c9f90e087c2795e62b9
TatGur <tatsiana.guryev@testpro.io>
2023-02-09 22:47:12 -0600

    25HW part 1
    problem with closeBrowser, codes working

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,41 +3,91 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.time.Duration;
 import java.util.UUID;
 
 
 public class BaseTest {
-    public static WebDriver driver = null;
-    public static String url = null;
-    public static WebDriverWait wait = null;
-    public static FluentWait fluentWait = null;
+    static WebDriver driver = null;
+    static String url = null;
+    static WebDriverWait wait = null;
+    static Actions actions = null;
+    static ThreadLocal<WebDriver>threadLocal = null;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            System.setProperty("webdriver.chrome","chromedriver.exe");
+        } else{
+            System.setProperty("webdriver.chrome.driver","chromedriver");
+        }
     }
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public static void launchBrowser(String BaseURL) {
+    public static void launchBrowser(String BaseURL) throws MalformedURLException {
         LoginTests.driver = new ChromeDriver();
-//        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
-        driver.get(url);
+        threadLocal = new ThreadLocal<>();
+        driver = pickBrowser(System.getProperty("browser"));
+        threadLocal.set(driver);
         wait = new WebDriverWait(LoginTests.driver, Duration.ofSeconds(20));
+        actions = new Actions(getDriver());
+        getDriver().manage().window().maximize();
+        getDriver().get(url);
+    }
+    public static WebDriver getDriver() {
+        return threadLocal.get();
     }
 
     @AfterMethod
     public static void closeBrowser(){
-        LoginTests.driver.quit();
+   //    LoginTests.driver.quit();
+        getDriver().quit();
+        threadLocal.remove();
+    }
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.160:4444";
+
+        switch(browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+  //          case "cloud":
+  //              return lambdaTest();
+            default:
+                WebDriverManager.chromedriver().setup();
+                return driver = new ChromeDriver();
+        }
     }
 
 //    protected static void navigateToPage() {
---
+++ src/test/java/TGHW25.java
@@ -0,0 +1,25 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class TGHW25 extends BaseTest{
+    @Test(dataProvider = "incorrectLoginProviders", dataProviderClass = BaseTest.class)
+    public static void loginEmptyEmailPasswordTest (String email, String password) {
+        login(email, password);
+        Assert.assertEquals(getDriver().getCurrentUrl(), url);
+    }
+    @Test(enabled = true, priority = 1, description = "LoginValidEmailValidPasswordTest")
+    public static void loginValidEmailValidPasswordTest () throws InterruptedException {
+        HomePage homePage = new HomePage(getDriver());
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        loginPage.logIn();
+        homePage.getUserAvatar();
+
+        Thread.sleep(5000);
+
+
+    }
+
+}
