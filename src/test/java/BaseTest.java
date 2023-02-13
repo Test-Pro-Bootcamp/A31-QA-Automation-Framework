eaf57362314bee71f6fdcdeef2fd8d2537fabf67
Anupa <anupagijo@live.com>
2023-02-13 06:46:45 -0800

    Homework24_Anupa
    Selenium Grid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,8 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -15,6 +18,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.UUID;
 
@@ -35,8 +40,8 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public static void launchBrowser(String BaseURL) {
-        LoginTests.driver = new ChromeDriver();
+    public static void launchBrowser(String BaseURL) throws MalformedURLException {
+        LoginTests.driver = pickBrowser(System.getProperty("browser"));
         //LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(BaseURL);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -46,6 +51,21 @@ public class BaseTest {
     @AfterMethod
     public static void closeBrowser(){
         LoginTests.driver.quit();
+    }
+
+    public static WebDriver pickBrowser(String brwsr) throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        String gridURL = "http://10.0.0.231:4444";
+        switch (brwsr) {
+            case "grid-chrome":
+                cap.setCapability("browserName","chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), cap);
+//                break;
+            default:
+                WebDriverManager.edgedriver().setup();
+                return driver=new EdgeDriver();
+        }
+//        return driver;
     }
 
     public static void navigateToPage()
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -42,29 +42,29 @@ public class LoginTests extends BaseTest {
 
     }
 
-    @Test(enabled = true, priority = 3, description = "LoginValidEmailValidPasswordTest")
-    public static void updateProfileNameTest () throws InterruptedException {
-
-        navigateToPage();
-
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
-
-        Thread.sleep(2000);
-        clickAvatarIcon();
-
-        String randomName = generateRandomName();
-
-        provideCurrentPassword("te$t$tudent");
-        provideProfileName(randomName);
-        clickSaveButton();
-
-        Thread.sleep(2000);
-        WebElement actualProfileName = driver.findElement(By.cssSelector("a.view-profile>span"));
-        Assert.assertEquals(actualProfileName.getText(), randomName);
-
-    }
+//    @Test(enabled = true, priority = 3, description = "LoginValidEmailValidPasswordTest")
+//    public static void updateProfileNameTest () throws InterruptedException {
+//
+//        navigateToPage();
+//
+//        provideEmail("demo@class.com");
+//        providePassword("te$t$tudent");
+//        clickSubmit();
+//
+//        Thread.sleep(2000);
+//        clickAvatarIcon();
+//
+//        String randomName = generateRandomName();
+//
+//        provideCurrentPassword("te$t$tudent");
+//        provideProfileName(randomName);
+//        clickSaveButton();
+//
+//        Thread.sleep(2000);
+//        WebElement actualProfileName = driver.findElement(By.cssSelector("a.view-profile>span"));
+//        Assert.assertEquals(actualProfileName.getText(), randomName);
+//
+//    }
 
 
 }
