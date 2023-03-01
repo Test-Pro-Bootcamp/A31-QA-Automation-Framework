25a1d19cc3bc8c501622a140d1965ab60d2de21e
Serge <serge.uwiduhaye@testpro.io>
2023-03-01 13:27:27 -0600

    Commit Homework29

--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -18,27 +18,4 @@ public class ProfileTests extends BaseTest {
         profilePage.chooseVioletTheme()
                 .isVioletThemeSelected();
     }
-
-    @Test(enabled = false, priority = 1, description = "update profile test")
-    public void updateProfileNameTest () throws InterruptedException {
-        LoginPage loginPage = new LoginPage(getDriver());
-
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
-
-        Thread.sleep(2000);
-        clickAvatarIcon();
-
-        String randomName = generateRandomName();
-
-        provideCurrentPassword("te$t$tudent");
-        provideProfileName(randomName);
-        clickSaveButton();
-
-        Thread.sleep(2000);
-        WebElement actualProfileName = getDriver().findElement(By.cssSelector("a.view-profile>span"));
-        Assert.assertEquals(actualProfileName.getText(), randomName);
-
-    }
 }
