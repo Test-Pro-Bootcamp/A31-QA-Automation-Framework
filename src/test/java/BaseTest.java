3187fcd36d0320da3fffa2d7182f051def70a2ec
nicoleliano <nicole.liano@testpro.io>
2023-03-11 10:02:12 -0500

    Jenkins

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -24,7 +24,7 @@ import java.util.UUID;
 public class BaseTest {
 
     WebDriver driver;
-    String url;
+    String url = "https://bbb.testpro.io/";;
     WebDriverWait wait;
     ThreadLocal<WebDriver> threadDriver;
 
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,6 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
+
+    @Test
+    public void verifyUserCanViewArtistPage(){
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.provideEmail("nicoleliano@testpro.io");
+        loginPage.providePassword("te$tpa$$word");
+    }
+
+
+
     @Test(dataProvider = "IncorrectLoginProvider", dataProviderClass = BaseTest.class)
     public void loginEmptyEmailPasswordTest (String email, String password) {
         LoginPage loginPage = new LoginPage(getDriver());
@@ -11,6 +22,7 @@ public class LoginTests extends BaseTest {
         loginPage.login(email, password);
         Assert.assertEquals(getDriver().getCurrentUrl(), url);
     }
+<<<<<<< Updated upstream
 
     @Test
     public void loginValidEmailValidPasswordTest (){
@@ -37,5 +49,33 @@ public class LoginTests extends BaseTest {
 
         Assert.assertEquals(getDriver().getCurrentUrl(), url);
     }
+=======
+//
+//    @Test
+//    public void loginValidEmailValidPasswordTest (){
+//
+//        LoginPage loginPage = new LoginPage(driver);
+//        HomePage homePage = new HomePage(driver);
+//
+//        loginPage.provideEmail("demo@class.com");
+//        loginPage.providePassword("te$t$tudent");
+//        loginPage.clickSubmitBtn();
+//
+//        homePage.getUserAvatar();
+//
+//    }
+//
+//    @Test
+//    public void loginInvalidEmailValidPasswordTest() {
+//
+//        LoginPage loginPage = new LoginPage(driver);
+//
+//        loginPage.provideEmail("invalid@class.com");
+//        loginPage.providePassword("te$t$tudent");
+//        loginPage.clickSubmitBtn();
+//
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//    }
+>>>>>>> Stashed changes
 
 }
