ca3e3b1bb8ee5cad8a9c5406cfc7eec62c53deab
AlexPaulavets <121207814+AlexPaulavets@users.noreply.github.com>
2023-04-03 14:08:38 -0500

    HW21 is done

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,7 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -19,6 +21,7 @@ public class BaseTest {
     public static String url = null;
     public static WebDriverWait wait = null;
     public static FluentWait fluentWait = null;
+    public static Actions actions = null;
 
     @BeforeSuite
     static void setupClass() {
@@ -26,14 +29,22 @@ public class BaseTest {
     }
 
     @BeforeMethod
+
     @Parameters({"BaseURL"})
     public static void launchBrowser(String BaseURL) {
-        LoginTests.driver = new ChromeDriver();
-//        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments ("--remote-allow-origins=*");
+        LoginTests.driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+
         url = BaseURL;
         driver.get(url);
         wait = new WebDriverWait(LoginTests.driver, Duration.ofSeconds(20));
+        actions = new Actions(driver);
     }
+
 
     @AfterMethod
     public static void closeBrowser(){
--- src/test/java/SeleniumTechniques.java
+++ src/test/java/SeleniumTechniques.java
@@ -1,11 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.List;
 
@@ -15,7 +15,7 @@ public class SeleniumTechniques extends BaseTest {
     //      context click (sometimes koel page does not load completely, re-run the test)
     @Test
     public void playSong()   {
-        login();
+        login("demo@class.com", "te$t$tudent");
         chooseAllSongsList();
         contextClickFirstSong();
         choosePlay();
@@ -25,17 +25,18 @@ public class SeleniumTechniques extends BaseTest {
     //      renames playlist using Actions double click (Pre-requisite - create at least one playlist)
     @Test
     public void renamePlaylist() throws InterruptedException {
-        login();
+        login("demo@class.com", "te$t$tudent");
         doubleClickChoosePlaylist();
+        //Thread.sleep(5000);
         enterPlaylistName();
         Assert.assertTrue(doesPlaylistExist());
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
 
     }
     //    displays all songs in the playlist (Pre-requisite - create at least one playlist)
     @Test
     public void listOfSongsWebElements() {
-        login();
+        login("demo@class.com", "te$t$tudent");
         choosePlaylist();
         displayAllSongs();
         Assert.assertTrue(getPlaylistDetails().contains(String.valueOf(countSongsInPlaylist())));
@@ -43,7 +44,7 @@ public class SeleniumTechniques extends BaseTest {
 
     @Test
     public void hoverOverPlayBtn() {
-        login();
+        login("demo@class.com", "te$t$tudent");
         chooseAllSongsList();
         hoverToPlayBtn();
         Assert.assertTrue(hoverToPlayBtn().isDisplayed());
@@ -59,7 +60,7 @@ public class SeleniumTechniques extends BaseTest {
     public void contextClickFirstSong() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
         WebElement firstSong = driver.findElement(By.cssSelector(".all-songs tr.song-item:nth-child(1)"));
-        action.contextClick(firstSong).perform();
+        actions.contextClick(firstSong).perform();
     }
 
     public void choosePlay() {
@@ -74,13 +75,14 @@ public class SeleniumTechniques extends BaseTest {
     public void doubleClickChoosePlaylist() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
         WebElement playlistElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
-        action.doubleClick(playlistElement).perform();
+        actions.doubleClick(playlistElement).perform();
     }
 
     public void choosePlaylist() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)"))).click();
     }
     public void enterPlaylistName() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
         WebElement playlistInputField = driver.findElement(By.cssSelector("input[name='name']"));
 //        clear() does not work, element has an attribute of "required"
 //            workaround is ctrl A (to select all) then backspace to clear then replace with new playlist name
@@ -89,7 +91,8 @@ public class SeleniumTechniques extends BaseTest {
         playlistInputField.sendKeys(Keys.ENTER);
     }
     public boolean doesPlaylistExist() {
-        WebElement playlistElement = driver.findElement(By.xpath("//a[text()='Edited Playlist Name']"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Summer Songs']")));
+        WebElement playlistElement = driver.findElement(By.xpath("//a[text()='Summer Songs']"));
         return playlistElement.isDisplayed();
     }
 
@@ -112,7 +115,7 @@ public class SeleniumTechniques extends BaseTest {
     public WebElement hoverToPlayBtn(){
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid='play-btn']")));
         WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
-        action.moveToElement(playButton).perform();
+        actions.moveToElement(playButton).perform();
         return  playButton;
 
 }
